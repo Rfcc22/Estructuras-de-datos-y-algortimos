4c569a1a58023e2c1598a0610408f6099d28bfc9
Rfcc22 <rfconchac@gmail.com>
2026-09-02 12:24:55 -0600

    Apuntes recursividad

--- JavaProyects/Apuntes/Recursividad/Recursividad1.java
+++ JavaProyects/Apuntes/Recursividad/Recursividad1.java
@@ -5,6 +5,7 @@ public class Recursividad1 {
     public static void main(String[] args) {
         //factorial recursivo
         System.out.println(factorial(5));
+        System.out.println(esPar(5));
     }
 
     public static double factorial(int n){
@@ -24,9 +25,29 @@ public class Recursividad1 {
     * factorial(1) = 1  * 1
     * */
 
+     static boolean esPar(int n){
+        if (n==0){
+            return true;
+        } else {
+            return esImpar(n-1);
+        }
+    }
+    static boolean esImpar(int n) {
+        if (n==0){
+            return false;
+        } else {
+            return esPar(n - 1);
+        }
+    }
 
-
-
-
-
+    /*
+     * Recursion indirecta
+     *
+     * esPar(5) =  5!=0 por lo que llama a es Impar()
+     * esImpar(4) = 4!=0 por lo que llama a esPar()
+     * esPar(3) =  3!=0 por lo que llama a es Impar()
+     * esImpar(2) = 2!=0 por lo que llama a esPar()
+     * esPar(1) =  1!=0 por lo que llama a es Impar()
+     * esImpar(0) = 0==0 por lo que regresa false
+     * */
 }
